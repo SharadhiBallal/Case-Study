ee2a1a267da010f1cb525f00af1a189e80d85bc2
SharadhiBallal <ballalsharadhi@gmail.com>
2019-11-27 20:55:55 +0300

    Update simulator.py
    updated Simulator

--- serverApp/simulator.py
+++ serverApp/simulator.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
-import requests as req
 
-def main(number):
-   for each in range(int(number)):
-      resp = req.get("http://127.0.0.1:8000/ping/")
-      print(resp.text)
+import asyncio
+import concurrent.futures
+import requests
+
+import asyncio
+import concurrent.futures
+import requests
+
+async def main(number):
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+
+        loop = asyncio.get_event_loop()
+        futures = [
+            loop.run_in_executor(
+                executor, 
+                requests.get, 
+                'http://127.0.0.1:8000/ping/'
+            )
+            for i in range(number)
+        ]
+        for response in await asyncio.gather(*futures):
+            print(response.text)
+
+
 
 
 if __name__ == '__main__':
     number = input("Enter the number of user: ") 
-    main(number)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(int(number)))
+
+
+
+
+
